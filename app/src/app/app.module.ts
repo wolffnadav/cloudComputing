1ad312be25829c2248bc36d8cfa1b2c1f19b47c5
wolffnadav <wolffnadav@gmail.com>
2020-08-04 10:07:51 +0300

    first app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
