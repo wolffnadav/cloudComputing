a944b46971e12d1d1868149a47e4cf57c1453862
wolffnadav <wolffnadav@gmail.com>
2020-08-07 17:46:16 +0300

    Sweet alerts added

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-home',
--- app/src/app/register-person/register-person.component.ts
+++ app/src/app/register-person/register-person.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-register-person',
   templateUrl: './register-person.component.html',
   styleUrls: ['./register-person.component.scss']
 })
-export class RegisterPersonComponent implements OnInit {
+export class RegisterPersonComponent {
 
   //User inforamtion
   private userPhoneNumber: String;
@@ -14,19 +15,37 @@ export class RegisterPersonComponent implements OnInit {
   private userEmail: String;
   private businessEntered: String;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   //when customer scans a barcode this adds his visit to the Users table
   insertNewPerson() {
-    this.http.post<any>('/api/insertNewPerson', {number: this.userPhoneNumber, username: this.userName, email: this.userEmail, business: this.businessEntered})
+    this.http.post<any>('/api/insertNewPerson', {
+      number: this.userPhoneNumber,
+      username: this.userName,
+      email: this.userEmail,
+      business: this.businessEntered
+    })
       .subscribe(data => {
         console.log(data.statusCode);
+        this.successAlert("You just managed to sign up :) ")
       }, error => {
-        console.error("insertNewPerson error: \n" + error.message)
+        console.error("insertNewPerson error: \n" + error.message);
+        this.failAlert();
+
       });
   }
 
-  ngOnInit() {
+  successAlert(message) {
+    Swal.fire("Wow", message, "success");
+  }
+
+  failAlert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!\nPlease try again..',
+    })
   }
 
 }
