4be08eae15df6a6a8a10c1fb8dc0d98ab76275a1
wolffnadav <wolffnadav@gmail.com>
2020-08-09 11:05:27 +0300

    Navbar Fixed

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,16 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule,MatSliderModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {AnalyticsComponent} from './analytics/analytics.component';
 import {RegisterBusinessComponent} from './register-business/register-business.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterPersonComponent } from './register-person/register-person.component';
-import { RegisterInfectedComponent } from './register-infected/register-infected.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {RegisterPersonComponent} from './register-person/register-person.component';
+import {RegisterInfectedComponent} from './register-infected/register-infected.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSliderModule,
-    BrowserAnimationsModule,
+    FormsModule
 
   ],
   providers: [],
